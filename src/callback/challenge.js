3f67cfd03100dc02e34d2f7aebbacbb42ebc77c6
kevindoblea1 <kevin.alvarenga93@gmail.com>
2023-03-06 20:57:31 -0600

    donde me quede

---
+++ src/callback/challenge.js
@@ -0,0 +1,9 @@
+
+const XMLHttpRequest = require('xmlhttprequest');
+//llamado de la API, al colocar en mayuscula una constante decimos que no va cambiar
+const API = 'https://api.escuelajs.co/api/v1/products';
+
+
+function fetchData(urlApi, callback) {
+    
+}
